4dde5fb4495b5a6f1a19fe185434005494d9e646
Wanwen Chen <aliciawwchen@gmail.com>
2019-03-14 16:53:04 +0800

    Update DecisionTree.py

--- Decision Tree/DecisionTree.py
+++ Decision Tree/DecisionTree.py
@@ -1,18 +1,15 @@
-# 参考：https://github.com/PnYuan/Machine-Learning_ZhouZhihua/tree/master/ch4_decision_tree
-# 将ID3和CART，剪枝融合在同一函数中了，通过在pruning中选择None,'pre','post'控制剪枝及criterion中选择'entropy''gini'选择划分依据
-# 未解决问题：堆栈溢出
-
 import re
 import math
 import random
+import numpy as np
 import pandas as pd
 import sys
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
 
 sys.setrecursionlimit(100000)
-threshold_entropy = 0.3
-threshold_gini = 0.4
+threshold_entropy = 0.1
+threshold_gini = 0.1
 
 
 class Node(object):
@@ -67,7 +64,7 @@ def TreePrePurning(trainSet, vldSet, criterion, isContinuous):
         if criterion == 'entropy' and gain < threshold_entropy:
             newNode.feature = None
             return newNode
-        elif criterion == 'gini' and gain > threshold_gini:
+        elif criterion == 'gini' and gain < threshold_gini:
             newNode.feature = None
             return newNode
 
@@ -142,7 +139,10 @@ def TreePostPurning(root, vldSet, isContinuous):
         valueCount = CountValue(vldSet[root.feature])
         for value in list(valueCount):
             subSet = vldSet[vldSet[root.feature].isin([value])]  # get sub set
-            accSub = TreePostPurning(root.leaves[value], subSet, isContinuous)
+            if value in root.leaves:
+                accSub = TreePostPurning(root.leaves[value], subSet, isContinuous)
+            else:
+                accSub = PredictAccuracy(root, subSet, isContinuous)
             if accSub == -1:  # -1 means no pruning back from this child
                 return -1
             else:
@@ -172,7 +172,7 @@ def TreeGenerate(dataSet, criterion, isContinuous):
         if criterion == 'entropy' and gain < threshold_entropy:
             newNode.feature = None
             return newNode
-        elif criterion == 'gini' and gain > threshold_gini:
+        elif criterion == 'gini' and gain < threshold_gini:
             newNode.feature = None
             return newNode
 
@@ -207,7 +207,7 @@ def Predict(root, sample, isContinuous):
                 key = ">%.3f" % divValue
                 root = root.leaves[key]
         else:
-            key = sample[root.feaeture].values[0]
+            key = sample[root.feature].values[0]
             if key in root.leaves:
                 root = root.leaves[key]
             else:
@@ -249,22 +249,28 @@ def CountValue(data):
 def BestFeature(dataSet, criterion, isContinuous):
     if criterion == 'entropy':
         gain = 0
+        bestFeature = None
+        divValue = 0
         for feature in dataSet.columns[1:-1]:
             tempGain, tempDiv = CalculateGain(dataSet, feature, isContinuous)
-            if tempGain > gain:
+            if tempGain >= gain:
                 gain = tempGain
                 bestFeature = feature
                 divValue = tempDiv
         return bestFeature, divValue, gain
     elif criterion == 'gini':
-        gini = float('Inf')
+        giniIndex = CalculateGini(dataSet[dataSet.columns[-1]])
+        gini = giniIndex
+        bestFeature = None
+        divValue = 0
         for feature in dataSet.columns[1:-1]:
             tempGini, tempDiv = CalculateGiniIndex(dataSet, feature, isContinuous)
-            if tempGini < gini:
+            if tempGini <= gini:
                 gini = tempGini
                 bestFeature = feature
                 divValue = tempDiv
-        return bestFeature, divValue, gini
+        gain = giniIndex - gini
+        return bestFeature, divValue, gain
     else:
         print('Wrong criterion!')
         bestFeature = -1
@@ -325,7 +331,7 @@ def CalculateGiniIndex(dataSet, feature, isContinuous):
         divValue, giniIndex = min(gini.items(), key=lambda x: x[1])
     else:
         data = dataSet[feature]
-        label = data[data.columns[-1]]
+        label = dataSet[dataSet.columns[-1]]
         valueCount = CountValue(data)
         for key in valueCount:
             keyLabel = label[data == key]
@@ -344,23 +350,30 @@ def CalculateGini(labels):
 
 
 if __name__ == "__main__":
+
     with open("data/iris.csv", mode='r') as data_file:
         dataSet = pd.read_csv(data_file, header=0)
-        n = len(dataSet[dataSet.columns[-1]])
-    # 10-fold cross validation
-    kf = KFold(n_splits=10)
+    n = len(dataSet[dataSet.columns[-1]])
     isContinuous = [True, True, True, True]
+
+    with open("data/hayes-roth.csv", mode='r') as data_file:
+        dataSet = pd.read_csv(data_file, header=0)
+    n = len(dataSet[dataSet.columns[-1]])
+    isContinuous = [False, False, False]
+
+    kf = KFold(n_splits=10)
+
     labels = dataSet[dataSet.columns[-1]]
     labelCount = CountLabels(labels)
     KFoldTrainSet = {}
-    KFoldTestSet={}
-    l = 0
+    KFoldTestSet = {}
+    flag = 0
     for label in labelCount:
         subSet = dataSet[dataSet[dataSet.columns[-1]] == label]
         n = len(subSet[subSet.columns[-1]])
         t = 0
         for train_index, test_index in kf.split(subSet):
-            if l == 0:
+            if flag == 0:
                 KFoldTestSet[t] = subSet.iloc[test_index]
                 KFoldTrainSet[t] = subSet.iloc[train_index]
                 t += 1
@@ -368,27 +381,37 @@ if __name__ == "__main__":
                 KFoldTestSet[t] = pd.concat([KFoldTestSet[t], subSet.iloc[test_index]])
                 KFoldTrainSet[t] = pd.concat([KFoldTrainSet[t], subSet.iloc[train_index]])
                 t += 1
-        if l == 0:
-            l = 1
-
+        if flag == 0:
+            flag = 1
+    acc_id3 = np.zeros([10, 3])
+    acc_cart = np.zeros([10, 3])
     for t in range(10):
         trainSet = KFoldTrainSet[t]
         testSet = KFoldTestSet[t]
         root = DecisionTree(trainSet, isContinuous, criterion='entropy')
         acc = PredictAccuracy(root, testSet, isContinuous)
+        acc_id3[t, 0] = acc
         print(acc)
         root = DecisionTree(trainSet, isContinuous, criterion='entropy', purning='pre')
         acc = PredictAccuracy(root, testSet, isContinuous)
+        acc_id3[t, 1] = acc
         print(acc)
         root = DecisionTree(trainSet, isContinuous, criterion='entropy', purning='post')
         acc = PredictAccuracy(root, testSet, isContinuous)
+        acc_id3[t, 2] = acc
         print(acc)
         root = DecisionTree(trainSet, isContinuous, criterion='gini')
         acc = PredictAccuracy(root, testSet, isContinuous)
+        acc_cart[t, 0] = acc
         print(acc)
         root = DecisionTree(trainSet, isContinuous, criterion='gini', purning='pre')
         acc = PredictAccuracy(root, testSet, isContinuous)
+        acc_cart[t, 1] = acc
         print(acc)
         root = DecisionTree(trainSet, isContinuous, criterion='gini', purning='post')
         acc = PredictAccuracy(root, testSet, isContinuous)
+        acc_cart[t, 2] = acc
         print(acc)
+    result = np.hstack([acc_id3, acc_cart])
+    df = pd.DataFrame(result, columns=["id3", "id3-pre", "id3-post", "cart", "cart-pre", "cart-post"])
+    df.to_csv("result.csv", index=True)
